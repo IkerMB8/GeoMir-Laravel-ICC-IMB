8a8512ea401b7ded5e6111a434a69ab46f719cf2
Iker <ikermbergua@gmail.com>
2022-10-25 16:12:41 +0200

    8.1 y 8.2

--- laravel/app/Http/Controllers/Auth/RegisterController.php
+++ laravel/app/Http/Controllers/Auth/RegisterController.php
@@ -8,6 +8,8 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Auth\Events\Registered;
 
 class RegisterController extends Controller
 {
@@ -64,11 +66,17 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
             'role_id' => 1,
         ]);
+        event(new Registered($user));
+
+        Auth::login($user);
+        $user->sendEmailVerificationNotification();
+        return $user;
+
     }
 }
