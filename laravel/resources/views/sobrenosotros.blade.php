5eed32d67cc7bd8b2170447d35e0b52d848a63bd
ikercc4 <ikercaste@gmail.com>
2022-12-16 18:00:30 +0100

    Video M09

--- laravel/resources/views/sobrenosotros.blade.php
+++ laravel/resources/views/sobrenosotros.blade.php
@@ -1,6 +1,22 @@
 @extends('layouts.app')
 @section('content')
 <style>
+    video{
+        width: 100%;
+        height: 100%;
+    }
+    .h1video{
+        color:white;
+    }
+    .modal-content{
+        background-color: #1A1830 !important;
+    }
+    .modalBody{
+        display: flex;
+        justify-content:center;
+        flex-direction: column;
+    }
+
     .imgSNF{
         filter: grayscale(100%);
     }
@@ -10,6 +26,13 @@
     .imgSN:hover {
         width: 300px;
         transition: width 2s;
+    }
+
+    .btoVID{
+        padding:15px;
+        border-radius: 25px;
+        border: 2px solid black;
+        background-color:white;
     }
 
     @keyframes rotate {from {transform: rotate(0deg);}
@@ -49,29 +72,24 @@
             <div class="modal-dialog">
                 <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <h1 class="h1video" >VIDEO</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <button onclick="playVid()" type="button">Play Video</button>
-                    <button onclick="pauseVid()" type="button">Pause Video</button><br> 
+                    <div class="modalBody">
+                        <button onclick="playVid()" class="btoVID" type="button">Play Video</button>
+                        <button onclick="pauseVid()" class="btoVID" type="button">Pause Video</button>
+                    </div>
+                    <br>
+
+                    <br>
                     <video id="myVideo" width="320px" height="176px">
-                        <source src="mov_bbb.mp4" type="video/mp4">
-                        Your browser does not support HTML5 video.
+                        <source src="/img/iniesta.mp4" type="video/mp4">
                     </video>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
                 </div>
                 </div>
             </div>
-            </div>                                                                              
-
-
-
-
-
+            </div>  
 
             <h4 class="h4SN">Iker Castellano</h4>
             <div class="cambiar">
@@ -86,12 +104,33 @@
             </div>
         </div>
         <div class="fotosSN">
-            <img class="imgSN imgSNF" id="imagen2" src="img/iker.png"></img>
+            <button type="button" style="border:0; padding:0;" data-bs-toggle="modal" data-bs-target="#Modal">
+                <img class="imgSN imgSNF" id="imagen2" src="img/luispadrique.jpg"></img>
+            </button>
+            <div class="modal fade" id="Modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="h1video">VIDEO</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <button onclick="playVid2()" class="btoVID" type="button">Play Video</button>
+                    <button onclick="pauseVid2()" class="btoVID" type="button">Pause Video</button><br> 
+                    <br>
+                    <video id="myVideo2" width="320px" height="176px">
+                        <source src="/img/cocinero.mp4" type="video/mp4">
+                    </video>
+                </div>
+                </div>
+            </div>
+            </div>
+
             <h4 class="h4SN">Iker Martínez</h4>
             <div class="cambiar">
                 <a href="">
                 <big>
-                <p class="pSN">Co-Owner</p>
+                    <p class="pSN">Co-Owner</p>
                 </big>
                 <span>
                     <p class="pSN">Soy amante de los gatos</p>
@@ -150,14 +189,14 @@
     }
 
     function parar2F(){
-        document.getElementById('imagen2').src="/img/iker.png";
+        document.getElementById('imagen2').src="/img/luispadrique.jpg";
         bar.pause();
         imagen2.classList.add('imgSNF');
         imagen2.classList.remove('imgSNC');
     }
 
     function iniciar2F(){
-        document.getElementById('imagen2').src="/img/es.png";
+        document.getElementById('imagen2').src="/img/luispadrique.jpg";
         bar.play();
         imagen2.classList.remove('imgSNF');
         imagen2.classList.add('imgSNC');
@@ -173,6 +212,17 @@
 
     function pauseVid() { 
         vid.pause(); 
+    }
+
+
+    var vid2 = document.getElementById("myVideo2"); 
+
+    function playVid2() { 
+        vid2.play(); 
+    } 
+
+    function pauseVid2() { 
+        vid2.pause(); 
     } 
 </script>
 @vite('resources/js/bootstrap.js')
