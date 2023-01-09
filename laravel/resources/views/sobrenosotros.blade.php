c50d13480fcf1cab0c10f08cbe71add72011bb9c
Iker <ikermbergua@gmail.com>
2023-01-09 17:21:17 +0100

    edit sobre nosotros

--- laravel/resources/views/sobrenosotros.blade.php
+++ laravel/resources/views/sobrenosotros.blade.php
@@ -45,7 +45,7 @@
         to {transform: rotate(360deg);}}
     @-webkit-keyframes rotate {from {-webkit-transform: rotate(0deg);}
         to {-webkit-transform: rotate(360deg);}}
-    .imgSN:hover{
+    .cardgiratorio:hover{
         -webkit-animation: 3s rotate linear infinite;
         animation: 3s rotate linear infinite;
         -webkit-transform-origin: 50% 50%;
@@ -55,8 +55,116 @@
         -webkit-animation-direction: reverse;
         animation-direction: reverse;
     }
+    .cardgiratorio{
+        height: 508px;
+    }
     
 </style>
+<!-- Modal Iker C -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="h1video" >VIDEO</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <br>
+                <div id="carouselExampleControls" class="carousel slide" data-bs-ride="false">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <div class="botoneraVid">
+                                <div class="modalBody">
+                                    <button onclick="playVid1()" class="btoVID" type="button">Play Video</button>
+                                    <br>
+                                    <button onclick="pauseVid1()" class="btoVID" type="button">Pause Video</button>
+                                </div>
+                            </div>
+                            <br>
+                            <video id="myVideo1" width="320px" height="176px">
+                                <source src="/img/iniesta.mp4" type="video/mp4">
+                            </video>
+                        </div>
+                        <div class="carousel-item">
+                            <div class="botoneraVid">
+                                <div class="modalBody">
+                                    <button onclick="playVid2()" class="btoVID" type="button">Play Video</button>
+                                    <br>
+                                    <button onclick="pauseVid2()" class="btoVID" type="button">Pause Video</button>
+                                </div>
+                            </div>
+                            <br>
+                            <video id="myVideo2" width="320px" height="176px">
+                                <source src="/img/cocinero.mp4" type="video/mp4">
+                            </video>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modal Iker M -->
+<div class="modal fade" id="Modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="h1video">VIDEO</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <br> 
+                <div id="carouselExampleControls2" class="carousel" data-bs-ride="false">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <div class="botoneraVid">
+                                <div class="modalBody">
+                                    <button onclick="playVid3()" class="btoVID" type="button">Play Video</button>
+                                    <br>
+                                    <button onclick="pauseVid3()" class="btoVID" type="button">Pause Video</button>
+                                </div>
+                            </div>
+                            <br>
+                            <video id="myVideo3" width="320px" height="176px">
+                                <source src="/img/rajoy.mp4" type="video/mp4">
+                            </video>
+                        </div>
+                        <div class="carousel-item">
+                            <div class="botoneraVid">
+                                <div class="modalBody">
+                                    <button onclick="playVid4()" class="btoVID" type="button">Play Video</button>
+                                    <br>
+                                    <button onclick="pauseVid4()" class="btoVID" type="button">Pause Video</button>
+                                </div>
+                            </div>
+                            <br>
+                            <video id="myVideo4" width="320px" height="176px">
+                                <source src="/img/españoles.mp4" type="video/mp4">
+                            </video>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls2" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls2" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>  
+            </div>
+        </div>
+    </div>
+</div>
 <div class="contenidoSN">
     <div class="arriba">
         <h1 class="h1SN">Conócenos</h1>
@@ -68,148 +176,27 @@
         </audio>
     </div>
     <div class="abajo">
-        <div class="fotosSN">
+        <div class="card " style="width: 18rem;">
             <button type="button" style="border:0; padding:0;" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                <img class="imgSN imgSNF" id="imagen" src="img/iker.png"></img>
+                <img class="card-img-top cardgiratorio" id="imagen" src="img/iker.png" alt="Card image cap">
             </button>
-
-            <!-- Modal -->
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="h1video" >VIDEO</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <br>
-                    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="false">
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <div class="botoneraVid">
-                                    <div class="modalBody">
-                                        <button onclick="playVid1()" class="btoVID" type="button">Play Video</button>
-                                        <br>
-                                        <button onclick="pauseVid1()" class="btoVID" type="button">Pause Video</button>
-                                    </div>
-                                </div>
-                                <br>
-                                <video id="myVideo1" width="320px" height="176px">
-                                    <source src="/img/iniesta.mp4" type="video/mp4">
-                                </video>
-                            </div>
-                            <div class="carousel-item">
-                                <div class="botoneraVid">
-                                    <div class="modalBody">
-                                        <button onclick="playVid2()" class="btoVID" type="button">Play Video</button>
-                                        <br>
-                                        <button onclick="pauseVid2()" class="btoVID" type="button">Pause Video</button>
-                                    </div>
-                                </div>
-                                <br>
-                                <video id="myVideo2" width="320px" height="176px">
-                                    <source src="/img/cocinero.mp4" type="video/mp4">
-                                </video>
-                            </div>
-                        </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Previous</span>
-                        </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Next</span>
-                        </button>
-                        </div>
-                    
-                </div>
-                </div>
-            </div>
-            </div>  
-
-            <h4 class="h4SN">Iker Castellano</h4>
-            <div class="cambiar">
-                <a href="">
-                <big>
-                <p class="pSN">Co-Owner</p>
-                </big>
-                <span>
-                    <p class="pSN">Soy amante del fútbol</p>
-                </span>
-                </a>
+            <div class="card-body">
+                <h5 class="card-title">Iker Castellano</h5>
+                <p class="card-text" id="S1">Co-Owner</p>
+                <p class="card-text noshow" id="NS1">Amante de las pelotas</p>
             </div>
         </div>
-        <div class="fotosSN">
+        <div class="card " style="width: 18rem;">
             <button type="button" style="border:0; padding:0;" data-bs-toggle="modal" data-bs-target="#Modal">
-                <img class="imgSN imgSNF" id="imagen2" src="img/luispadrique.jpg"></img>
+                <img class="card-img-top cardgiratorio" id="imagen2" src="img/luispadrique.jpg" alt="Card image cap">
             </button>
-            <div class="modal fade" id="Modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="h1video">VIDEO</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <br> 
-                    <div id="carouselExampleControls2" class="carousel" data-bs-ride="false">
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <div class="botoneraVid">
-                                    <div class="modalBody">
-                                        <button onclick="playVid3()" class="btoVID" type="button">Play Video</button>
-                                        <br>
-                                        <button onclick="pauseVid3()" class="btoVID" type="button">Pause Video</button>
-                                    </div>
-                                </div>
-                                <br>
-                                <video id="myVideo3" width="320px" height="176px">
-                                    <source src="/img/rajoy.mp4" type="video/mp4">
-                                </video>
-                            </div>
-                            <div class="carousel-item">
-                                <div class="botoneraVid">
-                                    <div class="modalBody">
-                                        <button onclick="playVid4()" class="btoVID" type="button">Play Video</button>
-                                        <br>
-                                        <button onclick="pauseVid4()" class="btoVID" type="button">Pause Video</button>
-                                    </div>
-                                </div>
-                                <br>
-                                <video id="myVideo4" width="320px" height="176px">
-                                    <source src="/img/españoles.mp4" type="video/mp4">
-                                </video>
-                            </div>
-                        </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls2" data-bs-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Previous</span>
-                        </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls2" data-bs-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Next</span>
-                        </button>
-                        </div>
-                    
-                </div>
-                </div>
-            </div>
-            </div>
-
-            <h4 class="h4SN">Iker Martínez</h4>
-            <div class="cambiar">
-                <a href="">
-                <big>
-                    <p class="pSN">Co-Owner</p>
-                </big>
-                <span>
-                    <p class="pSN">Soy amante de los gatos</p>
-                </span>
-                </a>
+            <div class="card-body">
+                <h5 class="card-title">Iker Martinez</h5>
+                <p class="card-text" id="S2">Co-Owner</p>
+                <p class="card-text noshow" id="NS2">Paseador de caracoles</p>
             </div>
         </div>
     </div>
-
 </div>
 <script>
     var aud = document.getElementById("españa"); 
@@ -233,6 +220,8 @@
         aud.pause();
         imagen.classList.add('imgSNF');
         imagen.classList.remove('imgSNC');
+        document.getElementById('S1').classList.remove('noshow');
+        document.getElementById('NS1').classList.add('noshow');
     }
 
     function iniciarF(){
@@ -240,9 +229,9 @@
         aud.play();
         imagen.classList.remove('imgSNF');
         imagen.classList.add('imgSNC');
+        document.getElementById('S1').classList.add('noshow');
+        document.getElementById('NS1').classList.remove('noshow');
     }
-
-    
 
     
     var imagen2=document.getElementById('imagen2');
@@ -262,6 +251,8 @@
         bar.pause();
         imagen2.classList.add('imgSNF');
         imagen2.classList.remove('imgSNC');
+        document.getElementById('S2').classList.remove('noshow');
+        document.getElementById('NS2').classList.add('noshow');
     }
 
     function iniciar2F(){
@@ -269,8 +260,9 @@
         bar.play();
         imagen2.classList.remove('imgSNF');
         imagen2.classList.add('imgSNC');
+        document.getElementById('S2').classList.add('noshow');
+        document.getElementById('NS2').classList.remove('noshow');
     }
-
 
     var vid = document.getElementById("myVideo1"); 
 
@@ -292,7 +284,6 @@
         vid2.pause(); 
     }
 
-
     var vid3 = document.getElementById("myVideo3"); 
 
     function playVid3() { 
@@ -302,8 +293,6 @@
     function pauseVid3() { 
         vid3.pause(); 
     }
-
-
 
     var vid4 = document.getElementById("myVideo4"); 
 
