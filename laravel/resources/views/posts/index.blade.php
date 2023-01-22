dd840608dbb637ca19cd8ffca002d3635dc7f9ad
Iker <ikermbergua@gmail.com>
2023-01-22 02:01:59 +0100

    responsive

--- laravel/resources/views/posts/index.blade.php
+++ laravel/resources/views/posts/index.blade.php
@@ -48,7 +48,7 @@
                         </button>
                         <!-- Modal -->
                         <div class="modal fade" id="post{{ $post->id }}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                            <div class="modal-dialog modal-dialog-centered logreg">
+                            <div class="modal-dialog modal-dialog-centered logreg comentariosmodal">
                                 <div class="modal-content modalcntnt">
                                     <div class="commentizq">
                                         <div class="modal-body">
